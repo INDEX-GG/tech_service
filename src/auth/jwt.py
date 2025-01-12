33e61802dbb1270afd331faba928e1556b3a26d5
Maksim Markin <jhonnyamplituda@gmail.com>
2025-01-12 18:41:20 +0500

    feat: added app_confid to swagger settings

--- src/auth/jwt.py
+++ src/auth/jwt.py
@@ -5,12 +5,11 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
-from src.config import settings
 from src.auth.config import auth_config
 from src.auth.exceptions import AuthorizationFailed, AuthRequired, InvalidToken
 from src.auth.schemas import JWTData
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.SUB_DOMAIN}/api/v2/auth/tokens", auto_error=False)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v2/auth/tokens", auto_error=False)
 
 
 def create_access_token(
